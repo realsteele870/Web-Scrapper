59d01fa0c50b733085c2165625d532e3ce11adbe
realsteele870 <30958037+realsteele870@users.noreply.github.com>
2022-10-24 12:03:12 -0500

    Add files via upload

--- database scraper/WebScraperWithGUIDatabase.py
+++ database scraper/WebScraperWithGUIDatabase.py
@@ -7,6 +7,8 @@ from tkinter import ttk
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
+from PIL import ImageTk, Image
+from io import BytesIO
 pw="123pass"
 db ="gpu"
 def create_db_connection(host_name, user_name, user_password, db_name):
@@ -24,7 +26,7 @@ def create_db_connection(host_name, user_name, user_password, db_name):
 
     return connection
 
-connection = create_db_connection("34.136.219.87", "root", pw, db)
+#connection = create_db_connection("34.136.219.87", "root", pw, db)
 #State = NORMAL
 
 def execute_query(connection, query):
@@ -73,35 +75,66 @@ def is_number(s):
         return False
 def webScrape(searchItem):
     State=DISABLED
+    priceList = []
+    for item in priceList:
+        priceList.pop
     
 
     firstString = searchItem.get()
+    
 
     tempStr = firstString.replace(" ","+")
     URL = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + tempStr + "&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html5lib")
+    page = requests.get(URL,allow_redirects=False)
     
+    soup = BeautifulSoup(page.content, "html5lib")
+    page.close
+    export = str(soup)
     results = soup.find(id="srp-river-main")
+    
     fewArray = soup.find_all("span",class_="BOLD")
-    fewpos=0
+    fewposCheck=0
+    
+    
     #print(fewArray)
     for item in fewArray:
         #print(item.text)
         if item.text == "Results matching fewer words":
             fewpos=item
+            fewposCheck=1
+    for item in fewArray:
+        fewArray.pop
         
+    
     fewer =soup.find(class_="BOLD")
-    if fewpos !=0:
-        fewpos =fewpos.sourcepos
-    priceList = []
+   # if fewpos !=0:
+    #    fewpos =fewpos.sourceline
+    
     flag =1
     ignore =1
     item_elements = results.find_all("div", class_="s-item__info clearfix")
+    image = results.findAll('img')
+    print(image[0].attrs['src'])
+    imageStr.set(str(image[1].attrs['src']))
+    response = requests.get(imageStr.get())
+    img_data = response.content
+    New_img=ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+    panel.configure(image=New_img)
+    panel.image=New_img
+    
+    
+    #response = requests.get(imgtxt)
+    #img_data = response.content
+    #img=ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+    
+
+
+   
     for item_element in item_elements:
         if flag == 0:
             title_element = item_element.find("span", role="heading")
             price_element = item_element.find("span", class_="s-item__price")
+            
             price_element =price_element.text.strip()
             price_element=price_element.replace(",","")
             title_elementCheck=title_element
@@ -113,7 +146,16 @@ def webScrape(searchItem):
         
 
             count = 0
-
+            if fewposCheck==0:
+                fewpos = item_element
+            #print("few pos source: "+str(fewpos.sourceline)+" element source: "+str(title_elementCheck.sourceline))
+            if (fewpos.sourceline == title_elementCheck.sourcline and fewposCheck != 0):
+                for item in item_elements:
+                    item_elements.pop
+                print("reset!!!JL!:J!:!")
+                webScrape(searchItem)
+                return
+            
 
 
 
@@ -126,11 +168,16 @@ def webScrape(searchItem):
                     print("ignored: "+ title_element)
             
             realprice=price_element[1:]
-            print(title_element+" "+price_element+" source pos: "+str(title_elementCheck.sourcepos)+" few pos: "+str(fewpos))
+            
 
-            if is_number(realprice) and ignore ==1 and (title_elementCheck.sourcepos < fewpos or fewpos == 0):
+            
+            if((title_elementCheck.sourceline == fewpos.sourceline or fewposCheck == 0) and (title_elementCheck.sourcepos < fewpos.sourcepos or fewposCheck == 0)and(is_number(realprice) and ignore ==1)):
                 realprice=float(realprice)
                 priceList.append(realprice)
+                #print(title_element+" "+price_element+" source pos: "+str(title_elementCheck.sourceline)+" few pos: "+str(fewpos))                else:
+            
+                    
+
                 
         
             #print(title_element.text.strip())
@@ -148,24 +195,26 @@ def webScrape(searchItem):
     meanText.set("Average is: $"+str(mean))
     medianText.set("Median is: $"+ str(median))
 
-    if(mean >= 50 and median >=50 and cardSelect.get()==1 ):
-        create_gpu_insert = """
-        INSERT INTO GpuPrices(gpu_name, gpu_price)
-        VALUES("""+'"'+ firstString+'"'+", "+str(mean)+""");"""
-        print(create_gpu_insert)
-        connection = create_db_connection("34.136.219.87", "root", pw, db) # Connect to the Database
-        execute_query(connection, create_gpu_insert) # Execute our defined query
+ #   if(mean >= 50 and median >=50 and cardSelect.get()==1 ):
+  #      create_gpu_insert = """
+   #     INSERT INTO GpuPrices(gpu_name, gpu_price)
+    #    VALUES("""+'"'+ firstString+'"'+", "+str(mean)+""");"""
+     #   print(create_gpu_insert)
+      #  connection = create_db_connection("34.136.219.87", "root", pw, db) # Connect to the Database
+       # execute_query(connection, create_gpu_insert) # Execute our defined query
     for item in priceList:
         priceList.pop
     priceList=[]
+    for item in item_elements:
+         item_elements.pop
     
     return
 
     
 root=Tk()
 root.title("Ebay Web Scraper")
-root.geometry('600x300')
-root.resizable(FALSE,False)
+root.geometry('900x400')
+root.resizable(TRUE,True)
 frm = ttk.Frame(root, padding=2)
 frm.grid()
 product = StringVar(frm)
@@ -175,8 +224,20 @@ meanText.set("Average is: ")
 medianText = StringVar(frm)
 medianText.set("Median is: ")
 cardSelect = IntVar(frm)
+ImageByte = IntVar(frm)
+imageStr = StringVar(frm)
+imageStr.set("https://ir.ebaystatic.com/rs/v/fxxj3ttftm5ltcqnto1o4baovyl.png")
+imgtxt="https://ir.ebaystatic.com/rs/v/fxxj3ttftm5ltcqnto1o4baovyl.png"
+response = requests.get(imageStr.get())
+img_data = response.content
 
 
+img=Image.open("empty.jpg")
+newImage=img.resize((50,50), Image.ANTIALIAS)
+Imagettk=ImageTk.PhotoImage(newImage)
+panel = ttk.Label(frm,image=Imagettk)
+panel.grid(column=4,row=0)
+#panel.pack(side="bottom",fill="both",expand="yes")
 Entrybox = ttk.Entry(frm,textvariable=product).grid(column=0,row=0,  padx=200,pady=50)
 
 searchButton =ttk.Button(frm, text="Search",command=lambda: webScrape(product),state=State).grid(column = 0, row= 1,pady=10)
@@ -184,10 +245,13 @@ searchButton =ttk.Button(frm, text="Search",command=lambda: webScrape(product),s
 
 
 
-meanLabel = ttk.Label(frm,textvariable=meanText).grid(column=0,row=2)
-medianLabel = ttk.Label(frm,textvariable=medianText).grid(column=0,row=3)
+meanLabel = ttk.Label(frm,textvariable=meanText)
+meanLabel.grid(column=0,row=2)
+medianLabel = ttk.Label(frm,textvariable=medianText)
+medianLabel.grid(column=0,row=3)
 
-cardRadio=ttk.Checkbutton(frm,text="Is this a gpu?", variable=cardSelect).place(x=400,y=50)
+cardRadio=ttk.Checkbutton(frm,text="Is this a gpu?", variable=cardSelect)
+cardRadio.place(x=400,y=50)
 
 
 
